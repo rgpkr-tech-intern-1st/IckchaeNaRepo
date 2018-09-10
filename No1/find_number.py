7a6ef4aef602d029e914a0dd682d04cc69c31595
RubikHouse <ni37fsl@gmail.com>
2018-09-10 10:26:25 +0900

    Use snake case and local variables

--- No1/find_number.py
+++ No1/find_number.py
@@ -1,33 +1,27 @@
 import subprocess, sys
 
-lowInt = -sys.maxint
-highInt = sys.maxint
 
-greater = "The value is greater than your guess.\n"
-less = "The value is less than your guess.\n"
-
-
-def binary_search(guess_num):
-    global lowInt
-    global highInt
+def binary_search(low_int, high_int):
+    guess_num = (low_int + high_int) / 2
 
     result = subprocess.check_output(["python", "guess.zip", str(guess_num)])
 
     if result == greater:
-        lowInt = guess_num
-        mid = (lowInt + highInt) / 2
-        binary_search(mid)
+        low_int = guess_num
+        binary_search(low_int, high_int)
 
     elif result == less:
-        highInt = guess_num
-        mid = (highInt + lowInt) / 2
-        binary_search(mid)
+        high_int = guess_num
+        binary_search(low_int, high_int)
 
     else:
         print(result)
 
 
-binary_search(0)
+greater = "The value is greater than your guess.\n"
+less = "The value is less than your guess.\n"
+
+binary_search(-sys.maxint, sys.maxint)
 
 
 
