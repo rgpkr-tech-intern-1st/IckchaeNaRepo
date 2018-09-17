ab687bd6cb8a7ad44399094bcf23a051d1c8d56d
RubikHouse <ni37fsl@gmail.com>
2018-09-17 09:52:22 +0900

    tearDown 수정, 임의의 작은 수(55,100)에 대한 테스트 케이스 추가, 임의의 수를 테스트하기 위한 binary_search_little_num 추가

--- No1/find_number.py
+++ No1/find_number.py
@@ -6,7 +6,7 @@ import find_number_const as const
 def binary_search(low_int, high_int):
     guess_num = (low_int + high_int) / 2
 
-    result = subprocess.check_output(["python", "guess.zip", str(guess_num)])
+    result = subprocess.check_output(["/usr/bin/python", "guess.zip", str(guess_num)])
 
     if result == const.GREATER:
         low_int = guess_num
@@ -18,6 +18,21 @@ def binary_search(low_int, high_int):
 
     else:
         return guess_num
+
+
+def binary_search_little_num(low_int, high_int, hidden_number):
+    guess_num = (low_int + high_int) / 2
+
+    if guess_num == hidden_number:
+        return guess_num
+
+    elif guess_num < hidden_number:
+        low_int = guess_num
+        return binary_search_little_num(low_int, high_int, hidden_number)
+
+    elif guess_num > hidden_number:
+        high_int = guess_num
+        return binary_search_little_num(low_int, high_int, hidden_number)
 
 
 if __name__ == '__main__':
--- No1/test_find_number.py
+++ No1/test_find_number.py
@@ -7,13 +7,26 @@ class TestFindNumber(unittest.TestCase):
     def setUp(self):
         self.low_int = -sys.maxint
         self.high_int = sys.maxint
-        self.n = 1954421783993232787
+        self.n = 0
 
     def tearDown(self):
-        print("Success")
+        self.low_int = None
+        self.high_int = None
+        self.n = None
 
     def test_find_number(self):
+        self.n = 1954421783993232787
         guess_num = fn.binary_search(self.low_int, self.high_int)
+        self.assertEqual(self.n, guess_num)
+
+    def test_find_even_num(self):
+        self.n = 100
+        guess_num = fn.binary_search_little_num(self.low_int, self.high_int, self.n)
+        self.assertEqual(self.n, guess_num)
+
+    def test_find_odd_num(self):
+        self.n = 55
+        guess_num = fn.binary_search_little_num(self.low_int, self.high_int, self.n)
         self.assertEqual(self.n, guess_num)
 
 
